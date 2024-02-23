af3dc9b7d47f8865e90dc4fb1ff91718eacf8a03
AkashKumarGit <54853342+AkashKumarGit@users.noreply.github.com>
2024-02-23 23:39:06 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,99 @@
+# Importing libraries
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+import matplotlib.pyplot as plt
+
+# Defining device
+use_cuda = torch.cuda.is_available()
+device = torch.device("cuda" if use_cuda else "cpu")
+device
+
+# Train data transformations
+train_transforms = transforms.Compose([
+    transforms.RandomApply([transforms.CenterCrop(22), ], p=0.1),
+    transforms.Resize((28, 28)),
+    transforms.RandomRotation((-15., 15.), fill=0),
+    transforms.ToTensor(),
+    transforms.Normalize((0.1307,), (0.3081,)),
+    ])
+
+# Test data transformations
+test_transforms = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.1407,), (0.4081,))
+    ])
+
+# Create train and test data
+train_data = datasets.MNIST('../data', train=True, download=True, transform=train_transforms)
+test_data = datasets.MNIST('../data', train=False, download=True, transform=test_transforms)
+
+# Create train and test loader
+batch_size = 512
+
+kwargs = {'batch_size': batch_size, 'shuffle': False, 'num_workers': 2, 'pin_memory': True}
+
+test_loader = torch.utils.data.DataLoader(test_data, **kwargs)
+train_loader = torch.utils.data.DataLoader(train_data, **kwargs)
+
+
+# Plot data images
+batch_data, batch_label = next(iter(train_loader))
+
+fig = plt.figure()
+
+for i in range(12):
+  plt.subplot(3,4,i+1)
+  plt.tight_layout()
+  plt.imshow(batch_data[i].squeeze(0), cmap='gray')
+  plt.title(batch_label[i].item())
+  plt.xticks([])
+  plt.yticks([])
+
+  
+  # Define neural network structure
+class Net(nn.Module):
+    #This defines the structure of the NN.
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3)
+        self.conv4 = nn.Conv2d(128, 256, kernel_size=3)
+        self.fc1 = nn.Linear(4096, 50)
+        self.fc2 = nn.Linear(50, 10)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x), 2)
+        x = F.relu(F.max_pool2d(self.conv2(x), 2))
+        x = F.relu(self.conv3(x), 2)
+        x = F.relu(F.max_pool2d(self.conv4(x), 2))
+        x = x.view(-1, 4096)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+    
+# Data to plot accuracy and loss graphs
+train_losses = []
+test_losses = []
+train_acc = []
+test_acc = []
+
+test_incorrect_pred = {'images': [], 'ground_truths': [], 'predicted_vals': []}
+
+
+# Initialize neural network and perform training
+model = Net().to(device)
+optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=15, gamma=0.1, verbose=True)
+# New Line
+criterion = nn.CrossEntropyLoss()
+num_epochs = 20
+
+for epoch in range(1, num_epochs+1):
+  print(f'Epoch {epoch}')
+  train(model, device, train_loader, optimizer, criterion)
+  test(model, device, test_loader, criterion)
+  scheduler.step()
